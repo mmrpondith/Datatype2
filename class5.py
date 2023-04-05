cf2c26922a9a58c3a4486aad07c7df4a0e782f1b
“mmrpondith” <itibdd@gmail.com>
2023-04-05 15:57:02 +0600

    DataType

---
+++ class5.py
@@ -0,0 +1,63 @@
+#convert from int to float:
+#x = float(0)
+#convert from float to int:
+#y = int(2.8)
+#convert from int to complex:
+#z = complex(0)
+#print(x)
+#print(y)
+#print(z)
+#print(type(x))
+#print(type(y))
+#print(type(z))
+
+#x = int(1)
+#y = int(2.8)
+#z = str("5")
+#print(x)
+#print(y)
+#print(z)
+#x = float(10)
+#y = float(2.8)
+#z = float("3")
+#w = float("4.2")
+#print(x)
+#print(y)
+#print(z)
+#print(w)
+#x = str("s1")
+#y = str(2)
+#z = str(3.0)
+#print(x)
+#print(y)
+#print(z)
+#a = "Mahabub"
+#print(a)
+#A=input("your name : ")
+#print(A)
+#a = """Lorem ipsum dolor sit amet,
+#consectetur adipiscing elit,
+#sed do eiusmod tempor "incididunt"
+#ut labore et dolore magna aliqua.
+#ut labore et dolore magna aliqua."""
+#print(a)
+#a = '''Lorem ipsum dolor sit amet,
+#"consectetur" adipiscing elit,
+#sed do eiusmod tempor "incididunt"
+#ut labore et dolore magna aliqua.'''
+#print(a)
+
+#txt = "The best things in life are free!"
+#print("Habib" in txt)
+
+#txt = "The best things in life are free!"
+#if "in" in txt:
+#  print("Yes, 'in' is present.")
+#txt = "The best habbib things in life are free!"
+#print("habbib" not in txt)
+
+
+
+
+
+
